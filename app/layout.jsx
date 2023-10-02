3d87a0ca24bb6830a9da5747b6087887bbec6d68
sonurajp <sonurajp49@gmail.com>
2023-10-02 15:28:19 +0530

    Nav bar setup

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import "@styles/globals.css";
-
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 export const metadata = {
   title: "Promptopia",
   description: "Discover & Share Prompt",
@@ -12,7 +13,10 @@ export default function RootLayout({ children }) {
         <div className="main">
           <div className="gradient" />
         </div>
-        <main className="app">{children}</main>
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
       </body>
     </html>
   );
--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Feed from "@components/Feed";
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
@@ -12,6 +12,7 @@ const Home = () => {
         Promptopia is a open-source AI prompting tool for modern world to
         discover, create and share creative prompts
       </p>
+      <Feed />
     </section>
   );
 };
