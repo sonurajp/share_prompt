1e9daf1f4a1c2ca76e90dad891eba2abcd9ce7c6
sonurajp <sonurajp49@gmail.com>
2023-10-21 23:43:04 +0530

    some change

--- components/provider.jsx
+++ components/provider.jsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
-
 const Provider = ({ children, session }) => (
   <SessionProvider session={session}>{children}</SessionProvider>
 );
